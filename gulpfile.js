955999bc98a3ba7b9831bccb31c3879a085cfb3b
212417951 <katherine.menkhaus@ge.com>
2017-01-09 15:05:05 -0800

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+'use strict';
+const path = require('path');
+const gulp = require('gulp');
+const pkg = require('./package.json');
+const $ = require('gulp-load-plugins')();
+const gulpSequence = require('gulp-sequence');
+const importOnce = require('node-sass-import-once');
+const stylemod = require('gulp-style-modules');
+const browserSync = require('browser-sync').create();
+const gulpif = require('gulp-if');
+const combiner = require('stream-combiner2');
+const bump = require('gulp-bump');
+const argv = require('yargs').argv;
+
+const sassOptions = {
+  importer: importOnce,
+  importOnce: {
+    index: true,
+    bower: true
+  }
+};
+
+gulp.task('clean', function() {
+  return gulp.src(['.tmp', 'css'], {
+    read: false
+  }).pipe($.clean());
+});
+
+function handleError(err){
+  console.log(err.toString());
+  this.emit('end');
+}
+
+gulp.task('serve', function() {
+  browserSync.init({
+    port: 8080,
+    notify: false,
+    reloadOnRestart: true,
+    logPrefix: `${pkg.name}`,
+    https: false,
+    server: ['./', 'bower_components'],
+  });
+
+  gulp.watch(['css/*-styles.html', '*.html', '*.js']).on('change', browserSync.reload);
+
+});
+
+gulp.task('default', function(callback) {
+  gulpSequence('clean')(callback);
+});
